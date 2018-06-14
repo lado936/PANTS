81052b9c0d425d04eeefda691bf2bfb5fb257e51
Grace Daher <grace@daher.net>
2018-06-14 11:38:41 -0400

    added test for match_mats

--- tests/testthat/test_pants.R
+++ tests/testthat/test_pants.R
@@ -54,7 +54,7 @@ test_that('mat_pow > 2', {
   mat4pow <- mat %*% mat
   mat4pow <- mat4pow %*% mat
   mat4pow <- mat4pow %*% mat
-  expect_equal(PANTS:::mat_pow(mat,4),mat4pow)
+  expect_equal(mat_pow(mat,4),mat4pow)
 })
 
 test_that("edgelist -> kernel", {
@@ -84,6 +84,29 @@ test_that("gmt2Gmat", {
 test_that("match_mats", {
   expect_gt(length(intersect(rownames(G), colnames(kk))), 0)
   expect_equal(sum(G), 6)
+  
+  M2 <- M[,pheno != 'trt2']
+  pheno2 <- pheno[pheno != 'trt2']
+  score_fcn <- identity
+  contrast.v2 <- contrast.v[1]
+  nperm <- 10
+  score.v <- score_features(object= M2, phenotype.v= pheno2, contrast.v= contrast.v2, score_fcn= score_fcn )
+  
+  #feature scores in permutations, 74% dense but later combine with a sparse (empty) matrix
+  score.mat <- Matrix::Matrix(0, nrow=nrow(M2), ncol=nperm, dimnames = list(rownames(M2), paste0('perm', 1:nperm)))
+  for (perm in 1:nperm){
+    #must set permuted names to NULL st limma_contrasts doesn't complain thay they clash with colnames(object)
+    pheno.tmp <- stats::setNames(pheno2[sample(1:length(pheno2))], nm=NULL)
+    
+    score.mat[,perm] <- score_features(object=M2, phenotype.v=pheno.tmp, contrast.v=contrast.v2, score_fcn=score_fcn)
+
+  }
+  
+  mm <- match_mats(score.mat = cbind(v=score.v, score.mat), ker=kk, Gmat=G)
+  
+  expect_equal(dimnames(mm$score.mat)[1],dimnames(mm$ker)[1])
+  expect_equal(dimnames(mm$score.mat)[1],dimnames(mm$Gmat)[1])
+  
 })
 
 test_that("pants", {
